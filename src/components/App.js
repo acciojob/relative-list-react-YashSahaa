a609d8aa6e19826a3c42aebe3c15e1a5ba0186cf
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-27 08:12:02 +0530

    Submission1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import RelativeComponent from './relativeComponent'
 
 const App = () => {
+  const relative = ["Ram" , "Shyam" , "Sita" , "Geeta"];
+
+  const rc = relative.map((name , index)=>{
+    return <RelativeComponent key={name + index} name={name}/>
+  })
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <h1>Relative List</h1>
+        <ol>
+          {rc}
+        </ol>
     </div>
   )
 }
