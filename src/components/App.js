286d0d1a2aab47cdfaea859b5efa2411cafa4dbe
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-06-27 08:17:50 +0530

    Submission2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const relative = ["Ram" , "Shyam" , "Sita" , "Geeta"];
 
   const rc = relative.map((name , index)=>{
-    return <RelativeComponent key={name + index} name={name}/>
+    return <RelativeComponent key={"relativeListItem"+ index} name={name}/>
   })
   return (
     <div id="main">
